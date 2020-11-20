30cd75fe9893721e23b8bb36126eb56df85b042e
Ifex <if3cofr3sh@gmail.com>
2020-11-20 13:37:21 +0100

    created a static server

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 //create a route
-app.get('/', (req, res) => {
-	res.send('<h1>Hello Express</h1>');
-})
+/*app.get('/', (req, res) => {
+	// res.send('<h1>Hello Express.js</h1>');
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})*/
+
+// Set static folder for static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000; //normally this should be in a separate config file
 
